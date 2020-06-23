d5c0a83234ea8ebda7da68ebd831cc06f5370166
WA <waqarabbas66@gmail.com>
2020-06-23 11:40:40 +0500

    ForgotPassword Screen added

--- lib/Screens/homePage.dart
+++ lib/Screens/homePage.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:intelogsapp/organization/skillsGroup.dart';
+import 'package:intelogsapp/organization/SkillGroup/skillsGroup.dart';
 import 'package:intelogsapp/widgets/gridBox.dart';
 
 
